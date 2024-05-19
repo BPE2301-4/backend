6b03b2042e4feb8b0ec497a1db2533464a192b3a
ilya <pisi4nica@icloud.com>
2024-05-19 14:04:34 +0300

    Сделал проверку текста
    Теперь всё работает , принимает в себя любой рускоязычный текст и выдает JSON объект с исправленными текстом и списком ошибок . Так же обновил токен для OpenAI API

--- src/analysis/text_updater.py
+++ src/analysis/text_updater.py
@@ -1,5 +1,6 @@
 # import external libraries
 from openai import OpenAI
+import json
 # import from folder 'src/' via __init__
 from src import TOKEN
 
@@ -13,9 +14,13 @@ def mist_correction(text: str):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Ты - бот , который отлично знает русский язык. Твоя задача - находить ошибки в получеенном тобой тексте, исправлять их и отдельно выписывать те места и слова, в которых были допущены ошибки."},
+            {"role": "system", "content": "Ты - бот , который отлично знает русский язык . Твоя задача - находить ошибки в полученном тобой тексте , исправлять их и отдельно выписывать те места и слова, в которых были допущены ошибки . Тебе следует JSON-объект , где по ключу text будет строка , содержащая исправленный текст , а по ключу mistakes - Python список , содержащий в сеебе все слова , в которых допущены ошибки , в виде строк ."},
             {"role": "user", "content": text}
         ]
     )
-    if completion.choices[0].message is not None:
-        return completion.choices[0].message.content
+    response_text = completion.choices[0].message.content
+    try:
+        response = json.loads(response_text)
+        return response
+    except json.JSONDecodeError:
+        return None
